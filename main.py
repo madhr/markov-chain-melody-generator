6318291997a973efbb67cc7d69431d5c8752f5c1
Magda <magdahrebecka@gmail.com>
2020-03-06 15:43:08 +0000

    update main file draft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mido import MidiFile
-
+import datetime
+from pathlib import Path
 from main.converter import Converter
 
 if __name__ == '__main__':
@@ -9,21 +10,17 @@ if __name__ == '__main__':
 	track = track_list[3]
 
 	converter = Converter()
-	new_track = converter.filter_note_on_and_not_drums(track)
 
-	lowest_note = converter.get_lowest_note(new_track)
-	print("lowest_note", lowest_note)
+	generated_track = converter.generate_track(track)
 
-	chords = converter.extract_chords(new_track)
-	print("chords", chords)
+	outfile = MidiFile()
+	outfile.ticks_per_beat = 1200
 
-	substract_by = (lowest_note * (-1))
-	chords_normalized = converter.add_to_all_elements_in_list_of_lists(chords, substract_by)
-	print("chords normalized", list(chords_normalized))
+	outfile.tracks.append(generated_track)
 
-	converter.sort_elements_in_list_of_lists(chords_normalized)
+	file_dir = "results/"
+	file_name = str(datetime.datetime.now())
+	Path(file_dir).mkdir(exist_ok=True)
+	file_path = file_dir + '/' + file_name + '.mid'
+	outfile.save(file_path)
 
-	dict = converter.group_into_pairs(chords_normalized)
-	for key, value in dict.items():
-		print("key", key)
-		print("value", value)
